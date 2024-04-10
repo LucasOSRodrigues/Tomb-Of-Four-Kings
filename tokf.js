95b1702101613e25fb81279004c4bd806d8eb9ce
Lucas O. S. Rodrigues <lucasosrodrigues13@gmail.com>
2024-04-09 22:34:59 -0300

    JS & CSS update
    Inicio do desenvolvimento do JS:
    - mudança no HTML e CSS pelo JS
    Melhoria no visual

--- tokf.js
+++ tokf.js
@@ -1,17 +1,18 @@
 //* HTML content.
 const torches = document.querySelector(".torches")
 const rooms = document.querySelector(".rooms")
+const roomsLeft = document.querySelector("#roomsLeft")
 const delve = document.querySelector(".delve")
 const retreat = document.querySelector(".retreat")
-const buttons = document.querySelector(".buttons")
 const hand = document.querySelector(".hand")
 const HP = document.querySelector(".HP")
 
-//* Code var's.
-const deck = []
 let hp = 9
+drawHP()
+moveGhost()
 
 //? Gerador de baralho.
+const deck = []
 for (let index = 2; index <= 10; index++) {
   deck.push({ type: "encounter", suit: "door", value: index })
   deck.push({ type: "encounter", suit: "monster", value: index })
@@ -26,8 +27,17 @@ for (let suit of ["door", "monster", "trap", "hearts"]) {
 deck.push({ type: "treasure", suit: "joker", value: "scroll" })
 deck.sort(() => (Math.random() > 0.5 ? 1 : -1))
 
-function createCard() {
-  const card = document.createElement("div")
-  card.className = "card"
+function drawHP() {
+  HP.innerHTML = ""
+  for (let S2 = 0; S2 < hp; S2++) HP.innerHTML += heart
 }
-
+function moveGhost() {
+  const ghost = document.createElement("div")
+  ghost.className = "ghost"
+  ghost.innerHTML = plus
+  ghost.onclick = () => {}
+  delve.append(ghost)
+}
+function del() {
+  delve.removeChild(document.querySelector(".ghost"))
+}
