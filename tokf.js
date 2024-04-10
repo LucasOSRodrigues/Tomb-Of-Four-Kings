2b195c9a0ca308ae1ccf375e92a765daeb0ee9ed
Lucas O. S. Rodrigues <lucasosrodrigues13@gmail.com>
2024-04-10 15:53:38 -0300

    Uptdate JS
    Atualização na entrega de carta

--- tokf.js
+++ tokf.js
@@ -1,15 +1,18 @@
 //* HTML content.
-const torches = document.querySelector(".torches")
-const rooms = document.querySelector(".rooms")
-const roomsLeft = document.querySelector("#roomsLeft")
-const delve = document.querySelector(".delve")
-const retreat = document.querySelector(".retreat")
-const hand = document.querySelector(".hand")
+const TORCHES = document.querySelector(".torches")
+const DECK = document.querySelector(".deck")
+const ROOMS_LEFT = document.querySelector("#roomsLeft")
+const DELVE = document.querySelector(".delve")
+const RETREAT = document.querySelector(".retreat")
+const HAND = document.querySelector(".hand")
 const HP = document.querySelector(".HP")
 
 let hp = 9
+let direction = DELVE
+let turn = 0
+let retreatTurn
 drawHP()
-moveGhost()
+createGhost()
 
 //? Gerador de baralho.
 const deck = []
@@ -21,23 +24,98 @@ for (let index = 2; index <= 10; index++) {
 for (let suit of ["door", "monster", "trap", "hearts"]) {
   deck.push({ type: "treasure", suit: suit, value: "skill" })
   deck.push({ type: "treasure", suit: suit, value: "king" })
-  deck.push({ type: "divinity", suit: suit, value: "divinity" })
-  deck.push({ type: "torch", suit: suit, value: "torch" })
+  deck.push({ type: "auto", suit: suit, value: "divinity" })
+  deck.push({ type: "auto", suit: suit, value: "torch" })
 }
 deck.push({ type: "treasure", suit: "joker", value: "scroll" })
 deck.sort(() => (Math.random() > 0.5 ? 1 : -1))
 
 function drawHP() {
   HP.innerHTML = ""
-  for (let S2 = 0; S2 < hp; S2++) HP.innerHTML += heart
+  for (let S2 = 0; S2 < hp; S2++)
+    setTimeout(() => {
+      HP.innerHTML += heart
+    }, S2 * 80)
 }
-function moveGhost() {
-  const ghost = document.createElement("div")
-  ghost.className = "ghost"
-  ghost.innerHTML = plus
-  ghost.onclick = () => {}
-  delve.append(ghost)
+
+function createGhost() {
+  const GHOST = document.createElement("div")
+  GHOST.className = "ghost"
+
+  const GO_BTN = document.createElement("div")
+  GO_BTN.onclick = delveDungeon
+  GO_BTN.innerHTML = plus
+  GHOST.append(GO_BTN)
+
+  if (direction === DELVE && turn > 1) {
+    const RETURN_BTN = document.createElement("div")
+    RETURN_BTN.onclick = retreatDungeon
+    RETURN_BTN.innerHTML = retreat
+    RETURN_BTN.id = "return_btn"
+    GHOST.append(RETURN_BTN)
+  }
+  direction.append(GHOST)
 }
-function del() {
-  delve.removeChild(document.querySelector(".ghost"))
+
+function deleteGhost() {
+  direction.removeChild(document.querySelector(".ghost"))
+}
+
+function dealCards() {
+  shiftedCard = deck.shift()
+  const card = document.createElement("div")
+  const plusDiv = document.createElement("div")
+  card.className = "card front"
+  card.innerHTML = handleSVG(shiftedCard)
+  card.append(plusDiv)
+  direction.append(card)
+}
+
+function delveDungeon() {
+  turn++
+  deleteGhost()
+  flipLastCard()
+  dealCards()
+  if (turn !== retreatTurn * 2 - 1) {
+    createGhost()
+  }
+}
+
+function flipLastCard() {
+  let lastCard = direction.lastChild
+  if (lastCard) {
+    lastCard.innerHTML = ""
+    lastCard.className = "card back"
+  }
+}
+
+function retreatDungeon() {
+  retreatTurn = turn
+  turn++
+
+  showRetreatCard()
+  deleteGhost()
+  flipLastCard()
+  direction = RETREAT
+  showRetreatLane()
+  dealCards()
+
+  if (turn > 2) {
+    if (turn !== retreatTurn * 2 - 1) {
+      createGhost()
+    }
+  } else {
+    flipLastCard()
+  }
+}
+
+function showRetreatLane() {
+  RETREAT.style.height = "var(--cardHeight)"
+  RETREAT.style.visibility = "visible"
+}
+function showRetreatCard() {
+  const retreatCard = document.createElement("div")
+  retreatCard.innerHTML = retreat
+  retreatCard.className = "retreatCard"
+  RETREAT.append(retreatCard)
 }
