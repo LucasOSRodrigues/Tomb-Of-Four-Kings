6903ee8619ac3e6fcb0bfd7f556b914f64878fc2
Lucas O. S. Rodrigues <lucasosrodrigues13@gmail.com>
2024-04-15 11:00:20 -0300

    Bugs corrigidos
    Correção de bugs
    Adição de consequências diferentes para cada tipo de encontro.

--- tofk.js
+++ tofk.js
@@ -1,4 +1,3 @@
-//* HTML content.
 const TORCHES = document.querySelector(".torches")
 const HAND = document.querySelector(".hand")
 const DELVE = document.querySelector(".delve")
@@ -8,26 +7,24 @@ const ROOMS_LEFT = document.querySelector("#roomsLeft")
 const TREASURE = document.querySelector(".treasure")
 const ACTION = document.querySelector(".action")
 const HP = document.querySelector(".HP")
-
 const time = 800
 
-ROOMS_LEFT.onclick = () => {
-  for (let i = 0; i < 16; i++) {
-    dealMainCard(deck.shift())
-  }
+function swap(x, y) {
+  ;[deck[x], deck[y]] = [deck[y], deck[x]]
 }
 
 let uncollectedTreasure = []
 let lane = DELVE
 let retreatTurn = null
 let [
+  cardOnDeck,
   turn,
   unusedDivinity,
   torchCounter,
   actionHand,
   collectedGold,
   lastDamageInstance,
-] = [0, 0, 0, 0, 0, 0]
+] = [0, 0, 0, 0, 0, 0, 0]
 
 let hp = 9 - lastDamageInstance
 lockAction = false
@@ -35,7 +32,6 @@ lockAction = false
 drawHP()
 createGhost()
 
-//* Gerador de baralho.
 const deck = []
 for (let suit of ["door", "monster", "trap", "heart"]) {
   deck.push({ type: "treasure", suit: suit })
@@ -49,15 +45,20 @@ for (let index = 2; index <= 10; index++) {
   deck.push({ type: "encounter", suit: "trap", value: index })
 }
 deck.push({ type: "treasure", suit: "scroll" })
-deck.sort(() => (Math.random() > 0.5 ? 1 : -1))
 
-console.log(deck)
+let randomNum = Math.round(Math.random() * 44)
+for (let scrambler = 0; scrambler < randomNum; scrambler++) {
+  deck.sort(() => (Math.random() > 0.5 ? 1 : -1))
+}
+console.log(deck, randomNum)
 
 for (let cardAmount = 0; cardAmount < deck.length; cardAmount++) {
   setTimeout(() => {
+    cardOnDeck++
     ROOMS_LEFT.innerText = cardAmount + 1
   }, (cardAmount * 1000) / deck.length)
 }
+
 function drawHP() {
   HP.innerHTML = ""
   for (let S2 = 0; S2 < hp; S2++)
@@ -138,6 +139,7 @@ function flipLastCard() {
 }
 
 function dealMainCard(shiftedCard) {
+  undrawCard()
   switch (shiftedCard.type) {
     case "encounter":
       const card = document.createElement("div")
@@ -176,7 +178,7 @@ function dealMainCard(shiftedCard) {
       const autoCard = document.createElement("div")
       autoCard.innerHTML = handleSVG(shiftedCard)
       if (shiftedCard.suit === "torch") {
-        autoCard.className = "flexCard front"
+        autoCard.className = "quarterCard front"
         torchCounter++
         TORCHES.append(autoCard)
       } else {
@@ -205,9 +207,8 @@ function addCardOn(encounterCard) {
       flipLastCard()
       handleDivinity(encounterCard)
       addCardOn()
-
-      collectGold(encounterCard)
     } else if (deck[0].type === "encounter") {
+      undrawCard()
       let shiftedCard = deck.shift()
       actionHand = shiftedCard.value
       const Card = document.createElement("div")
@@ -251,7 +252,7 @@ function handleEncounter(encounterCard, actionValue) {
       ACTION.innerHTML = ""
       TREASURE.innerHTML = ""
       lastDamageInstance = encounterCard.value - actionValue
-      undrawHP(lastDamageInstance)
+      undrawHP(encounterCard, lastDamageInstance)
       uptadeEncounterCard(encounterCard, actionValue)
       uncollectedTreasure = []
     } else {
@@ -267,31 +268,55 @@ function handleEncounter(encounterCard, actionValue) {
   }, time)
 }
 
-function undrawHP(damage) {
-  for (let lostS2 = 0; lostS2 < damage; lostS2++) {
-    setTimeout(() => {
-      try {
-        HP.firstChild.remove(HP.firstChild.lastChild)
-        --hp === 0 ? (HP.className = " card hud") : false
-      } catch (e) {}
-    }, (lostS2 * 1000) / damage)
-  }
+function undrawHP(encounterCard, damage) {
+  if (encounterCard.suit === "monster" || encounterCard.suit === "trap")
+    for (let lostS2 = 0; lostS2 < damage; lostS2++) {
+      setTimeout(() => {
+        try {
+          HP.firstChild.remove(HP.firstChild.lastChild)
+          --hp === 0 ? (HP.className = " card hud") : false
+        } catch (e) {}
+      }, (lostS2 * 1000) / damage)
+    }
 }
 function uptadeEncounterCard(encounterCard, damage) {
-  lane.lastChild.remove(lane.lastChild)
+  if (encounterCard.suit === "monster") {
+    lane.lastChild.remove(lane.lastChild)
 
-  encounterCard.value -= damage
-  const card = document.createElement("div")
-  const plusDiv = document.createElement("div")
-  card.className = "card front"
-  card.innerHTML = handleSVG(encounterCard)
-  plusDiv.innerHTML = plus
-  card.append(plusDiv)
-  card.onclick = () => addCardOn(encounterCard) //addCardOn não serve
-  card.innerHTML += handleValue(encounterCard)
+    encounterCard.value -= damage
+    const card = document.createElement("div")
+    const plusDiv = document.createElement("div")
+    card.className = "card front"
+    card.innerHTML = handleSVG(encounterCard)
+    plusDiv.innerHTML = plus
+    card.append(plusDiv)
+    card.onclick = () => addCardOn(encounterCard)
+    card.innerHTML += handleValue(encounterCard)
 
-  lockAction ? (lockAction = false) : false
-  lane.append(card)
+    lockAction ? (lockAction = false) : false
+    lane.append(card)
+  } else if (encounterCard.suit === "trap") {
+    flipLastCard()
+    if (turn !== retreatTurn * 2 - 1) {
+      createGhost()
+    }
+  } else {
+    let roomsLost = encounterCard.value - damage
+    for (let cardsLost = 0; cardsLost < roomsLost; cardsLost++) {
+      setTimeout(() => {
+        deck.shift()
+        undrawCard()
+      }, cardsLost * (1000 / damage))
+    }
+  }
+  flipLastCard()
+  if (turn !== retreatTurn * 2 - 1) {
+    createGhost()
+  }
+}
+
+function undrawCard() {
+  ROOMS_LEFT.innerHTML -= 1
 }
 
 function collectGold(trapCard) {
@@ -311,12 +336,10 @@ function collectGold(trapCard) {
 function collectTreasure() {
   if (TREASURE.innerHTML) {
     for (let treasure of uncollectedTreasure) {
-      if (treasure.cardSuit !== "king") {
-        treasure.onclick = () => {
-          console.log(treasure)
-        }
-        HAND.append(treasure)
+      treasure.onclick = () => {
+        console.log(treasure)
       }
+      HAND.append(treasure)
     }
     uncollectedTreasure = []
     TREASURE.innerHTML = ""
